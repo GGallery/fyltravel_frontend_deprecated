ad26031083bba998e834c10b298ae8b8ee167c47
AntonioGiangrave <tony@bslt.it>
2017-09-21 00:25:32 +0200

    create scatole vuote

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,17 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { BookComponent } from './book/book.component';
 import {BookService} from "./book/book.service";
+import { UserComponent } from './user/user.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BookComponent,
+    UserComponent,
+    HeaderComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
