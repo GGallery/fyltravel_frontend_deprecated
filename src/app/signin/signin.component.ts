921f994417a8a7da90c752cb851f4d9a28cbeb85
GGallery <antonio@ggallery.it>
2017-10-13 12:42:45 +0200

    aggiunto signin e signout funzionanti

--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-signin',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SigninComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+  }
+
+  onSignin(form: NgForm) {
+    this.authService.signin(form.value.email, form.value.password).
+      subscribe(
+      tokenData => console.log(tokenData),
+      error => console.log(error),
+    );
   }
 
 }
